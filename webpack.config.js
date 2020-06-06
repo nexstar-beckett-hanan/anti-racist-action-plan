caf16c7f022bb3ca63076d66ee9f332dd0c3e49c
Beckett Hanan <beckett.hanan@gmail.com>
2020-06-05 17:06:02 -0700

    fixing entry point for webpack

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: path.join(__dirname, '/src'),
+  entry: path.join(__dirname, '/src/'),
   output: {
     path: path.join(__dirname, '/build/'),
     filename: 'bundle.js',
