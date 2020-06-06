97c7ad4409e4dd1c6e8959ac0a2b8680f90266b9
Beckett Hanan <beckett.hanan@gmail.com>
2020-06-05 17:07:14 -0700

    reverting entry point for webpack back to main app file

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: path.join(__dirname, '/src/'),
+  entry: path.join(__dirname, '/src/app.jsx'),
   output: {
     path: path.join(__dirname, '/build/'),
     filename: 'bundle.js',
