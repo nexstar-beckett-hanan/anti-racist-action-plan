519e120e0e3149543ee5fc889bab717f0ca35887
Beckett Hanan <beckett.hanan@gmail.com>
2020-07-09 20:59:49 -0700

    editing comments for clarity

--- src/components/Action.js
+++ src/components/Action.js
@@ -1,20 +1,24 @@
 import React, { Component } from 'react';
 const parse = require('html-react-parser');
 
-const Action = ({ fetchedActions, actions }) => {
+const Action = ({ fetchedActions, actions, selectedAction }) => {
+  // if fetchedActions is true, we should have actions
   if (fetchedActions) {
+    // check to make sure the actions array is not empty
     if (!actions) {
+      // if it is empty, display that we couldn't find any actions
       return (
         <div id="action-box">
           <h2>BASED ON YOUR ANSWERS, YOUR PLAN OF ACTION IS:</h2>
           <div id="action">
-            <p>Sorry, no actions found</p>
+            <p>Sorry, we couldn't find any actions for you!</p>
           </div>
         </div>
       )
-    } else {
+    } else { // otherwise, we've got at least one action to work with. awesome!
+      // randomize which action we'll present to the user 
       const selectedAction = actions[Math.floor(Math.random() * actions.length)].action_text;
-  
+      // then render the action to the front-end
       return (
         <div id="action-box">
           <h2>BASED ON YOUR ANSWERS, YOUR PLAN OF ACTION IS:</h2>
--- src/components/Prompt.js
+++ src/components/Prompt.js
@@ -2,8 +2,6 @@ import React, { Component } from 'react';
 const parse = require('html-react-parser');
 
 const Prompt = ({ prompts, activePrompt }) => {
-  // javascript can go here if we need to process anything before rendering
-
   // this is what gets rendered to the front-end
   return prompts[activePrompt];
 }
